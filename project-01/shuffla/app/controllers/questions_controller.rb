7df96d8901b0417c7b566893c0e948df1c96cb81
fai.weiner <fai.weiner@gmail.com>
2014-06-30 18:43:39 +1000

    Got the song names to appear on New Question page

--- project-01/shuffla/app/controllers/questions_controller.rb
+++ project-01/shuffla/app/controllers/questions_controller.rb
@@ -1,10 +1,23 @@
 class QuestionsController < ActionController::Base
   def new
     require 'rspotify'
-    @artists = RSpotify::Artist.search("genre:Rock", limit = 10, offset = 0)
-
+    @artists = RSpotify::Artist.search("genre:Pop/Rock", limit = 2, offset = 0)
+    @song_list = []
     @artists.each do |artist|
-      # get song
+      top_tracks = artist.top_tracks(:US)
+      top_tracks.map do |t|
+        @song_list << t.id
+      end
     end
+    @links = ''
+    @song_list.each do |song|
+      @links += song
+      @links += ',' unless song == @song_list.last
+    end
+    @song_question = @song_list.sample
+    @track = RSpotify::Track.find(@song_question)
+
+    q = Question.new
+
   end
 end
